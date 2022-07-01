59876e9c3ed178ab0a355633bbfd17dc18995494
Silvio Cesar Araujo <araujo.scesar@gmail.com>
2022-06-30 23:56:05 -0300

    acessando parametros da url

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Router, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 
 const routes: Routes = [
   { path: "primeira-pagina", component: PrimeiraPaginaComponent },
-  { path: "segunda-pagina", component: SegundaPaginaComponent }
+  { path: "segunda-pagina", component: SegundaPaginaComponent },
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent }
+  //{ path: "", redirectTo: "primeira-pagina", pathMatch: "full" },
+   //{ path: "**", component: PaginaNaoEncontradaComponent}
 ]
 
 @NgModule({
--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
-    SegundaPaginaComponent
+    SegundaPaginaComponent,
+    PaginaComParametrosComponent
   ],
   imports: [
     BrowserModule,
