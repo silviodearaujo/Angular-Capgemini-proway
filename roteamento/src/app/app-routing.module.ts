3d401df5d964e476d73132c96f79c70dadd44bf0
Silvio Cesar Araujo <araujo.scesar@gmail.com>
2022-07-01 00:14:01 -0300

    lazy-loading

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com
 const routes: Routes = [
   { path: "primeira-pagina", component: PrimeiraPaginaComponent },
   { path: "segunda-pagina", component: SegundaPaginaComponent },
-  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent }
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent },
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) }
   //{ path: "", redirectTo: "primeira-pagina", pathMatch: "full" },
    //{ path: "**", component: PaginaNaoEncontradaComponent}
 ]
